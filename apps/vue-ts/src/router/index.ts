89f36f81f99f0d135c0635c518c8acb8446ae562
linong <linong@kuaishou.com>
2021-12-13 18:16:04 +0800

    feat: 增加ts版本&three模块化管理

---
+++ apps/vue-ts/src/router/index.ts
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter);
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/home1',
+    name: 'Home1',
+    component: () => import(/* webpackChunkName: "ts-home" */ '../views/Home1.vue'),
+  },
+  {
+    path: '/threejs',
+    name: 'ThreeJs',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "ThreeJs" */ '../views/ThreeJs.vue'),
+  },
+  {
+    path: '/threejs1',
+    name: 'ThreeJs1',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "ts-ThreeJs" */ '../views/ThreeJs1.vue'),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ apps/vue-ts/src/views/Home1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+
+import {
+  Component, Ref, Vue, Watch,
+} from 'vue-property-decorator';
+
+@Component({
+    name: 'app-home',
+})
+export default class App extends Vue {
+
+}
+</script>
